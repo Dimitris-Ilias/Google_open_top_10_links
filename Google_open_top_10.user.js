9d36925f36b5104ac24185156eb9bedd1bf6a41f
JimTortex <dimitrisiliascs@gmail.com>
2016-09-09 12:56:29 +0300

    updated script

---
+++ Google_open_top_10.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        Google top 10
+// @namespace   mhtsos
+// @version     1
+// @include     http*www.google.*/search?*
+// @grant       none
+// ==/UserScript==
+
+function openTabs(){
+
+	var links = document.getElementsByClassName("g");
+
+	for (var i = 0; i < links.length; i++) {
+		window.open(links[i].getElementsByTagName("a")[0].href);
+	};
+
+}
+
+
+var link = document.createElement("a");//create image link and set position
+link.href = "#";
+link.style = "position:absolute;top:5em;left:100%;zIndex:99999;";
+var img = document.createElement("img");
+img.src = "https://cdn3.iconfinder.com/data/icons/tango-icon-library/48/preferences-system-windows-64.png"
+link.appendChild(img);
+document.getElementById("tophf").appendChild(link);
+
+link.onclick = function(){
+	openTabs();
+};
